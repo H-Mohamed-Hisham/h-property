a9b465eb062e8803dfe4ddb97d8f6f134742b544
H-Mohamed-Hisham <hmhwebdev@gmail.com>
2024-05-18 02:24:29 +0530

    Featured Properties Implemented

---
+++ app/api/properties/featured/route.js
@@ -0,0 +1,23 @@
+// Config
+import connectDB from "@/config/database";
+
+// Model
+import Property from "@/models/Property";
+
+// GET : /api/properties/featured
+export const GET = async (request) => {
+  try {
+    await connectDB();
+
+    const properties = await Property.find({ is_featured: true });
+
+    return new Response(JSON.stringify(properties), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something Went Wrong", {
+      status: 500,
+    });
+  }
+};
